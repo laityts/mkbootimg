272ed71e941d3b228c750faf6d05e8c82669d8e3
laityts <laityts@gmail.com>
2022-03-24 13:53:54 +0800

    使用ndk编译

---
+++ jni/Android.mk
@@ -0,0 +1,37 @@
+APP_PLATFORM := android-16
+APP_PIE      := true
+LOCAL_PATH   := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libmincrypt
+LOCAL_MODULE_FILENAME   := libmincrypt
+LOCAL_SRC_FILES         := libmincrypt/dsa_sig.c \
+                               libmincrypt/p256.c \
+                               libmincrypt/p256_ec.c \
+                               libmincrypt/p256_ecdsa.c \
+                               libmincrypt/rsa.c \
+                               libmincrypt/sha.c \
+                               libmincrypt/sha256.c
+LOCAL_C_INCLUDES        := mincrypt
+LOCAL_EXPORT_C_INCLUDES := mincrypt
+LOCAL_CFLAGS            :=  -ffunction-sections -O3
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := mkbootimg
+LOCAL_MODULE_FILENAME   := mkbootimg
+LOCAL_SRC_FILES         := mkbootimg.c
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)
+LOCAL_CFLAGS            := -ffunction-sections -O3
+LOCAL_LDFLAGS           := -static -Wl,--gc-sections -s
+LOCAL_STATIC_LIBRARIES := libmincrypt
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := unpackbootimg
+LOCAL_MODULE_FILENAME   := unpackbootimg
+LOCAL_SRC_FILES         := unpackbootimg.c
+LOCAL_C_INCLUDES        := $(LOCAL_PATH)
+LOCAL_CFLAGS            :=  -ffunction-sections -O3
+LOCAL_LDFLAGS           := -static -Wl,--gc-sections -s
+include $(BUILD_EXECUTABLE)
